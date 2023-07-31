ec37491e24d5f8e85c167b2a50ebdeecaaa15699
eli-martin414 <70294157+eli-martin414@users.noreply.github.com>
2023-07-30 20:18:27 -0500

    listener for clear completed button

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,8 +1,23 @@
+/*
+  ADD BUTTON
+*/
 // get the "add" button from the HTML document by its id
 var addButton = document.getElementById("add-button");
-// listen for the "add" button to be clicked
+// listen for the "add" button to be clicked, and run the function to add an item
 addButton.addEventListener("click", addToDoItem);
-// test that the "add" button is being properly listened for
+// temporary add function
 function addToDoItem() {
   alert("Add button clicked!");
+}
+
+/*
+  CLEAR COMPLETED BUTTON
+*/
+// get the "clear completed" button from the HTML
+var clearCompletedButton = document.getElementById("clear-completed-button");
+// listen for the "clear completed" button to be clicked, and run the function to clear an item
+clearCompletedButton.addEventListener("click", clearCompletedToDoItems)
+// temporary clear function
+function clearCompletedToDoItems() {
+  alert("Clear Completed button clicked!");
 }
