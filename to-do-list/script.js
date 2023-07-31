00c4b4e859e0ce3707e6908534dc5ff6378d1b8f
eli-martin414 <70294157+eli-martin414@users.noreply.github.com>
2023-07-30 20:30:10 -0500

    add listeners for empty and save list buttons

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -21,3 +21,27 @@ clearCompletedButton.addEventListener("click", clearCompletedToDoItems)
 function clearCompletedToDoItems() {
   alert("Clear Completed button clicked!");
 }
+
+/*
+  EMPTY LIST BUTTON
+*/
+// get the "empty list" button from the HTML
+var emptyListButton = document.getElementById("empty-button")
+// listen for the "empy list" button to be clicked, and run the function to empty the list
+emptyListButton.addEventListener("click", emptyList);
+// temporary empty list function
+function emptyList() {
+  alert("Empty List button clicked!");
+}
+
+/*
+  SAVE BUTTON
+*/ 
+// get the "save list" button from the HTML
+var saveListButton = document.getElementById("save-button");
+// listen for it to be clicked and then run the saveList function
+saveListButton.addEventListener("click", saveList);
+// temp save list function
+function saveList() {
+  alert("Save List button clicked!");
+}
